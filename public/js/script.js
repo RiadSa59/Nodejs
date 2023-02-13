ecd9ad23100699c86b6202bceca9723f54830c91
riad.sabir.etu <riad.sabir.etu@univ-lille.fr>
2023-02-13 20:29:47 +0100

    Rock Paper Scissor , Socket.io

---
+++ public/js/script.js
@@ -0,0 +1,282 @@
+const socket = io('http://localhost:3000');
+
+// DOM Elements
+const userList = document.getElementById('userList');
+const body = document.querySelector('body');
+const game = document.querySelector('.game');
+const username = document.querySelector('.username');
+const buttons = document.querySelectorAll('.choises button');
+const message = document.querySelector('.message');
+const playAgainBtn = document.querySelector('.play-again');
+const exit = document.querySelector('.exit');
+
+
+
+let name = prompt('Enter your name');
+while (!name) {
+  name = prompt('Enter your name');
+}
+
+const createPlayerElement = user => {
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  const div = document.createElement('div');
+  const btn = document.createElement('button');
+
+  btn.innerText = 'Play';
+  li.setAttribute('id', user.id);
+  btn.setAttribute('value', user.id);
+  btn.classList.add('play');
+  btn.disabled = user.status;
+  if (user.status) {
+    btn.style.display = 'none';
+  }
+  li.append(span);
+  div.classList.add('player');
+  div.textContent = `${user.name}`;
+  div.append(btn);
+  li.append(div);
+
+  userList.append(li);
+};
+
+socket.emit('new-user-connected', name);
+socket.emit('set-name', name);
+socket.on('get-all-users', data => {
+  username.textContent = `Username: ${name}`;
+
+  data.forEach(user => {
+    createPlayerElement(user);
+  });
+
+  socket.on('add-new-user', data => {
+    createPlayerElement(data);
+  });
+});
+
+socket.on('user-disconnect', user => {
+  document.getElementById(user).remove();
+});
+
+userList.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.classList.contains('play')) {
+    socket.emit('send-invite', e.target.value);
+  }
+});
+
+socket.on('invite', data => {
+  // Show popup for accept or refuse game invite
+  createPopup(data.name);
+
+  const btns = document.getElementById('popupBtns');
+  btns.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.innerText.toLowerCase() === 'accept') {
+      socket.emit('accept-invite', data);
+      document.querySelector('.popup').remove();
+      game.style.display = 'block';
+    } else {
+      socket.emit('refuse-invite', data.id);
+      document.querySelector('.popup').remove();
+    }
+  });
+});
+
+socket.on('refuse', name => {
+  const popup = document.createElement('div');
+  const text = document.createElement('p');
+  popup.classList.add('popup');
+  popup.classList.add('fadeOut');
+  text.textContent = `${name} refused your invite ☹`;
+  text.style.marginTop = '3rem';
+  popup.append(text);
+  body.append(popup);
+  setTimeout(() => {
+    document.querySelector('.popup').remove();
+  }, 4000);
+});
+
+socket.on('accepted-invite', data => {
+  socket.emit('join-room', data.room);
+  game.style.display = 'block';
+});
+
+socket.on('playing', data => {
+  const buttons = document.querySelectorAll('.play');
+
+  buttons.forEach(btn => {
+    data.forEach(id => {
+      if (btn.value === id) {
+        btn.disabled = true;
+        btn.style.display = 'none';
+      }
+    });
+  });
+});
+
+const createPopup = name => {
+  const popup = document.createElement('div');
+  const span = document.createElement('span');
+  const buttonDiv = document.createElement('div');
+  const accept = document.createElement('button');
+  const refuse = document.createElement('button');
+
+  popup.classList.add('popup');
+  popup.innerText = 'Game invite from ';
+
+  span.innerText = name;
+
+  accept.innerText = 'Accept';
+  accept.setAttribute('id', 'accept');
+  refuse.innerText = 'Refuse';
+  refuse.setAttribute('id', 'refuse');
+  buttonDiv.setAttribute('id', 'popupBtns');
+  buttonDiv.append(accept, refuse);
+
+  popup.append(span, buttonDiv);
+
+  body.append(popup);
+};
+
+// Game table
+const disableBtn = () => {
+  buttons.forEach(button => {
+    button.disabled = true;
+  });
+};
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', function() {
+    this.classList.add('selected');
+
+    socket.emit('send-choise', {
+      id: socket.id,
+      name: socket.name,
+      choise: this.getAttribute('value')
+    });
+
+    disableBtn();
+  });
+});
+
+const showOpponentChoise = image => {
+  const img = document.createElement('img');
+  img.src = `img/${image}.png`;
+
+  document.querySelector('.opponent').appendChild(img);
+};
+
+socket.on('get-choises', data => {
+  const res = data[1].filter(result => result.game === data[0]);
+
+  if (res.length > 1) {
+    const opponent = res.filter(opp => opp.user_id !== socket.id);
+    showOpponentChoise(opponent[0].choise);
+
+    const winner = getWinner(res);
+    showeWinnerMessage(winner);
+
+    socket.emit('clear-results', data[0]);
+  }
+});
+
+const getWinner = data => {
+  if (data[0].choise === data[1].choise) {
+    return 'draw';
+  } else {
+    if (data[0].choise === 'rock') {
+      if (data[1].choise === 'paper') {
+        return data[1].user_id;
+      } else {
+        return data[0].user_id;
+      }
+    }
+
+    if (data[0].choise === 'paper') {
+      if (data[1].choise === 'scissors') {
+        return data[1].user_id;
+      } else {
+        return data[0].user_id;
+      }
+    }
+    if (data[0].choise === 'scissors') {
+      if (data[1].choise === 'rock') {
+        return data[1].user_id;
+      } else {
+        return data[0].user_id;
+      }
+    }
+  }
+};
+
+const showeWinnerMessage = winner => {
+  let msg = '';
+  if (winner === 'draw') {
+    msg = 'Draw!';
+    message.classList.add('winner');
+  } else {
+    if (winner === socket.id) {
+      msg = 'Congratulations, You Won!';
+      message.classList.add('winner');
+    } else {
+      msg = 'You lose!';
+      message.classList.add('loser');
+    }
+  }
+
+  message.textContent = msg;
+
+  playAgainBtn.style.display = 'block';
+};
+
+playAgainBtn.addEventListener('click', () => {
+  document.querySelector('.selected').classList.remove('selected');
+  message.textContent = '';
+  message.classList.remove('winner');
+  message.classList.remove('loser');
+  document.querySelector('.opponent img').remove();
+  buttons.forEach(btn => {
+    btn.disabled = false;
+  });
+});
+
+exit.addEventListener('click', function() {
+  if (document.querySelector('.selected')) {
+    document.querySelector('.selected').classList.remove('selected');
+  }
+  message.textContent = '';
+  message.classList.remove('winner');
+  message.classList.remove('loser');
+
+  if (document.querySelector('.opponent img')) {
+    document.querySelector('.opponent img').remove();
+    buttons.forEach(btn => {
+      btn.disabled = false;
+    });
+  }
+
+  game.style.display = 'none';
+
+  socket.emit('leave-room', socket.id);
+});
+
+socket.on('end-game', id => {
+  const buttons = document.querySelectorAll('.play');
+
+  buttons.forEach(btn => {
+    if (btn.value === id) {
+      btn.disabled = false;
+      btn.style.display = 'block';
+    }
+  });
+});
+
+socket.on('opponent-left-game', () => {
+  message.textContent = `Your opponent left game!
+  (After 10 seconds your game will end)`;
+  message.classList.add('small');
+  setTimeout(() => {
+    exit.click();
+  }, 10000);
+});
---
+++ server.js
@@ -0,0 +1,116 @@
+const app = require('express')();
+const express = require('express');
+const dotenv = require('dotenv').config();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+server.listen(port);
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+let users = [];
+let results = [];
+io.on('connection', socket => {
+  socket.on('set-name', name => {
+    socket.name = name;
+    
+  });
+
+  socket.on('new-user-connected', data => {
+    const newUser = {
+      id: socket.id,
+      name: data,
+      status: false
+    };
+    users.push(newUser);
+
+    socket.broadcast.emit('add-new-user', newUser);
+  });
+
+  socket.emit('get-all-users', users);
+
+  socket.on('disconnect', () => {
+    users = users.filter(user => user.id !== socket.id);
+    socket.broadcast.emit('user-disconnect', socket.id);
+
+    const rooms = Object.keys(io.sockets.adapter.rooms);
+    rooms.forEach(room => {
+      if (room.includes(socket.id)) {
+        io.to(room).emit('opponent-left-game', 'Your opponent left room');
+      }
+    });
+  });
+
+  socket.on('send-invite', id => {
+    const player = users.map(user => user.id).indexOf(id);
+    if (users[player].status) {
+      return;
+    }
+
+    const roomName = `${id}-${socket.id}`;
+    io.to(id).emit('invite', {
+      name: socket.name,
+      id: socket.id,
+      room: roomName
+    });
+  });
+
+  socket.on('accept-invite', data => {
+    const player1 = users.map(user => user.id).indexOf(data.id);
+    const player2 = users.map(user => user.id).indexOf(socket.id);
+    users[player1].status = true;
+    users[player2].status = true;
+    io.sockets.emit('playing', [socket.id, data.id]);
+
+    socket.join(data.room);
+    io.to(data.id).emit('accepted-invite', {
+      id: socket.id,
+      room: data.room
+    });
+  });
+
+  socket.on('refuse-invite', id => {
+    io.to(id).emit('refuse', socket.name);
+  });
+
+  socket.on('join-room', room => {
+    socket.join(room);
+  });
+
+  socket.on('leave-room', id => {
+    const rooms = Object.keys(io.sockets.sockets[id].rooms).filter(
+      room => room !== socket.id
+    );
+
+    rooms.forEach(room => {
+      if (room !== socket.id) {
+        socket.leave(room);
+        io.to(room).emit('opponent-left-game', 'Your opponent left room');
+      }
+    });
+
+    const index = users.map(user => user.id).indexOf(id);
+    users[index].status = false;
+    io.sockets.emit('end-game', id);
+  });
+
+  socket.on('send-choise', data => {
+    const room = Object.keys(socket.rooms);
+
+    const choise = {
+      game: room[1],
+      name: socket.name,
+      choise: data.choise,
+      user_id: data.id
+    };
+
+    results.push(choise);
+    io.to(room[1]).emit('get-choises', [room[1], results]);
+  });
+
+  socket.on('clear-results', game => {
+    results = results.filter(res => res.game !== game);
+  });
+});
